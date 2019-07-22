0c3d770d4292e68544b6cce0bf4906688a7fd068
Saul Hdz Lara <knighthdz97@gmail.com>
2019-07-22 01:26:18 -0500

    Add function addItem
    - Fixed up Css.
    - Added some tags.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,9 @@
 // User click in the button Add
 document.getElementById('AddItem').addEventListener('click', function(){
-	console.log('button clicked');
 	var value = document.getElementById('Item').value;
 	if(value){
-		console.log(value);
+		addItem(value);
+		document.getElementById('Item').value = '';
 	}else{
 		$("body").overhang({
 		  type: "error",
@@ -12,3 +12,27 @@ document.getElementById('AddItem').addEventListener('click', function(){
 		});
 	}
 });
+
+// Function that allow add items in list
+function addItem(text) {
+	var list = document.getElementById('ToDo');
+
+	var item = document.createElement('li'); // Create a list
+	item.innerText = text;
+
+	var buttons = document.createElement('div'); // Create a div
+	buttons.classList.add('Buttons');				// Add a class
+
+	var remove = document.createElement('button'); // Create a button
+	remove.classList.add('Remove');
+	remove.innerHTML = '<span class="fas fa-trash"></span>';
+
+	var complete = document.createElement('button');
+	complete.classList.add('Complete');
+	complete.innerHTML = '<span class="far fa-check-square"></span>';
+
+	buttons.appendChild(remove);
+	buttons.appendChild(complete);
+	item.appendChild(buttons);
+	list.insertBefore(item, list.childNodes[0]); // Insert the item in first place
+}
