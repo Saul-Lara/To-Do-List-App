ac1695d450f0c4a9ff9f98d1ed4951144f39f631
Saul Hdz Lara <knighthdz97@gmail.com>
2019-07-23 00:22:12 -0500

    Add function removeItem
    - Add Confirmation Alert

--- Scripts/main.js
+++ Scripts/main.js
@@ -13,6 +13,26 @@ document.getElementById('AddItem').addEventListener('click', function(){
 	}
 });
 
+// Function that allow remove items of list
+function removeItem() {
+	var item = this.parentNode.parentNode // li tag (Item)
+	var parent = item.parentNode;  // accessing the list
+
+	$("body").overhang({
+	  type: "confirm",
+	  primary: "#40D47E",
+	  accent: "#27AE60",
+	  yesColor: "#3498DB",
+	  message: "Do you want to continue?",
+	  callback: function (value) {
+	    var response = value ? "Yes" : "No";
+	    if (response == "Yes") {
+	    	parent.removeChild(item); // Removing item of list
+	    }	    
+	  }
+	});
+}
+
 // Function that allow add items in list
 function addItem(text) {
 	var list = document.getElementById('ToDo');
@@ -26,6 +46,7 @@ function addItem(text) {
 	var remove = document.createElement('button'); // Create a button
 	remove.classList.add('Remove');
 	remove.innerHTML = '<span class="fas fa-trash"></span>';
+	remove.addEventListener('click', removeItem);	// Add click event for remove item
 
 	var complete = document.createElement('button');
 	complete.classList.add('Complete');
