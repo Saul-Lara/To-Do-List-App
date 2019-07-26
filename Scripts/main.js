e830661e91c74535ee808102446675d71e4345bd
Saul Hdz Lara <knighthdz97@gmail.com>
2019-07-26 00:30:07 -0500

    App finished
    - Add new JS functions.
    - Add new CSS styles.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,39 @@
+// Check if To Do List is available in localStorage and the data is added. if isn't available data is empty.
+// condition ? value_if_is_true : value_if_is_false
+var data = (localStorage.getItem('ToDoList')) ? JSON.parse(localStorage.getItem('ToDoList')) : {
+	ToDo:[],
+	Completed:[]
+};
+
+renderList();
+
 // User click in the button Add
 document.getElementById('AddItem').addEventListener('click', function(){
 	var value = document.getElementById('Item').value;
+	add(value);
+});
+
+// User press enter key in the input
+document.getElementById('Item').addEventListener('keydown', function(e){
+	var value = this.value;
+	if(e.code === 'Enter' || e.code === 'NumpadEnter'){
+		add(value);
+	}
+});
+
+// User click in the button Clear
+document.getElementById('DeleteItems').addEventListener('click', function(){
+	deleteList();
+});
+
+// Function that check if the input value is empty.
+function add(value) {
 	if(value){
 		addItem(value);
 		document.getElementById('Item').value = '';
+
+		data.ToDo.push(value); // Add a new item to array
+		dataUpdated();
 	}else{
 		$("body").overhang({
 		  type: "error",
@@ -11,13 +41,24 @@ document.getElementById('AddItem').addEventListener('click', function(){
 		  duration: 2
 		});
 	}
-});
+}
 
 // Function that allow change items to completed list or re-add to the to do list
 function completeItem() {
 	var item = this.parentNode.parentNode // li tag (Item)
 	var parent = item.parentNode;  // accessing the list
 	var id = parent.id; // Id of list
+	var value = item.innerText;
+
+	if(id === 'ToDo'){
+		data.ToDo.splice(data.ToDo.indexOf(value), 1);
+		data.Completed.push(value);
+	}else{
+		data.Completed.splice(data.Completed.indexOf(value), 1);
+		data.ToDo.push(value);
+	}
+
+	dataUpdated();
 
 	// Check if the item should be added to completed list or should be re-add to the to do list
 	// condition ? value_if_is_true : value_if_is_false
@@ -31,6 +72,8 @@ function completeItem() {
 function removeItem() {
 	var item = this.parentNode.parentNode // li tag (Item)
 	var parent = item.parentNode;  // accessing the list
+	var id = parent.id; // Id of list
+	var value = item.innerText;
 
 	$("body").overhang({
 	  type: "confirm",
@@ -40,7 +83,16 @@ function removeItem() {
 	  message: "Do you want to remove this item?",
 	  callback: function (value) {
 	    var response = value ? "Yes" : "No";
-	    if (response == "Yes") {
+		if (response == "Yes") {
+
+		   	if(id === 'ToDo'){
+				data.ToDo.splice(data.ToDo.indexOf(value), 1);
+			}else{
+				data.Completed.splice(data.Completed.indexOf(value), 1);
+			}
+
+			dataUpdated();
+
 	    	parent.removeChild(item); // Removing item of list
 	    }	    
 	  }
@@ -48,8 +100,8 @@ function removeItem() {
 }
 
 // Function that allow add items in list
-function addItem(text) {
-	var list = document.getElementById('ToDo');
+function addItem(text, completed = false) {
+	var list = (completed) ? document.getElementById('Completed') :document.getElementById('ToDo');
 
 	var item = document.createElement('li'); // Create a list
 	item.innerText = text;
@@ -71,4 +123,44 @@ function addItem(text) {
 	buttons.appendChild(complete);
 	item.appendChild(buttons);
 	list.insertBefore(item, list.childNodes[0]); // Insert the item in first place
+}
+
+// Function that allow save items in localstorage
+function dataUpdated() {
+	localStorage.setItem('ToDoList', JSON.stringify(data));
+}
+
+// Function that renders the list.
+function renderList() {
+	if(!data.ToDo.length && !data.Completed.length) return;
+
+	for (var i = 0; i < data.ToDo.length; i++) {
+		var value = data.ToDo[i];
+		addItem(value);
+	}
+
+	for (var j = 0; j < data.Completed.length; j++) {
+		var value = data.Completed[j];
+		addItem(value, true)
+	}
+}
+
+// Function that allow clear all items in localstorage
+function deleteList(){
+	if(!data.ToDo.length && !data.Completed.length)return;
+
+	$("body").overhang({
+	  type: "confirm",
+	  primary: "#ebae07",
+	  accent: "#c99400",
+	  yesColor: "#3498DB",
+	  message: "Do you want to remove all items?",
+	  callback: function (value) {
+	    var response = value ? "Yes" : "No";
+		if (response == "Yes") {
+			localStorage.removeItem('ToDoList');
+			location.reload();  	
+	    }	    
+	  }
+	});
 }
